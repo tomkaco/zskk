e7a37cff022a6e189021e951ced1b9af26c1fa50
tomkaco <tkaczmarek4@gmail.com>
2017-09-12 12:13:00 +0200

    gut gut

--- git/js/funkcje.js
+++ git/js/funkcje.js
@@ -14,3 +14,24 @@ console.log(poleProstokata(a, b));
 
 var name="Janusz";
 witaj(name);
+
+function poleObjetosc(szerokosc, dlugosc, wysokosc){
+    var pole = szerokosc * dlugosc;
+
+    var objetosc = pole * wysokosc;
+
+    var wynik = [pole, objetosc];
+    return wynik;
+
+}
+var a = prompt('Podaj szerokość')
+var b = prompt('Podaj dlugosc')
+var c = prompt('Podaj wysokosc')
+
+var x = prompt('Podaj wartość', '0 - pole', '1 - objetosc')
+var poleProstokata = poleObjetosc(2,3,4)[1];
+
+console.log(poleProstokata);
+
+
+
